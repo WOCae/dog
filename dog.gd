327074c4db3811762d04bd8046b878d1fe834756
WOCae <t-satoIZ@outlook.jp>
2024-02-29 19:30:56 +0900

    Update dog.gd

--- dog.gd
+++ dog.gd
@@ -1,9 +1,19 @@
 extends Area2D
 
+const SPEED := 15.0
+
 func _ready() -> void:
 	position = Vector2(400,500)	
 	scale =  Vector2(2,2)	
 
 func _process(delta: float) -> void:	
-	$AnimatedSprite2D.animation = &"walk"
+	#$AnimatedSprite2D.animation = &"walk"
+	#$AnimatedSprite2D.play()
+	var velocity = Vector2.ZERO	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1*delta
+		$AnimatedSprite2D.animation = &"walk"
+		$AnimatedSprite2D.flip_h = false	
+
+	position += velocity.normalized() * SPEED
 	$AnimatedSprite2D.play()
